2910147d64ec65133787c0191b14eeee9008a1a4
Eunchae <ddungal3@gmail.com>
2023-09-12 17:53:25 +0900

    20230912_solved

--- programmers/training/전국 대회 선발 고사/code.js
+++ programmers/training/전국 대회 선발 고사/code.js
@@ -1,16 +1,21 @@
-// rank와 attendance의 속성을 갖는는 객체를 만든다.
-// (attendance가 true인 학생들) 중에서
-// rank가 높은 순서대로 해당 학생들의 번호(인덱스): a,b,c
-// return  10000 × a + 100 × b + c;
-
 function solution(rank, attendance) {
-  const rankSet = rank
-    .map((rankValue, rankIdx) => ({ rankValue, rankIdx }))
-    .filter((_, idx) => attendance[idx]);
-
-  rankSet.sort((a, b) => a.rankValue - b.rankValue);
-  
-  const answer =
-    rankSet[0].rankIdx * 10000 + rankSet[1].rankIdx * 100 + rankSet[2].rankIdx;
-  return answer;
+    let a = 0, b = 0, c = 0;
+    let copiedRank = rank.slice(); // 배열 복사 (얕은 복사: 내부 객체나 배열은 원본과 공유되므로, 하나를 수정하면 다른 것도 영향을 받음)
+    
+    for(let i = 0; i < attendance.length; i++){
+        if(!attendance[i]){
+            copiedRank[i] = 100; // 결석한 학생의 순위(rank)는 100으로 바꿈
+        }
+    }
+    
+    // 오름차순 정렬
+    let setRanked = [...copiedRank].sort((a, b) => a - b);
+    
+    // a, b, c 값 계산
+    // 주의: 학생들의 번호는 0번부터 시작함
+    a = rank.indexOf(setRanked[0]);
+    b = rank.indexOf(setRanked[1]);
+    c = rank.indexOf(setRanked[2]);
+    
+    return 10000 * a + 100 * b + c;
 }
